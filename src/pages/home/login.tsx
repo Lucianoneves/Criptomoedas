e6af781f4f2fe98a35932c88a2bc52e5643ebb3a
luciano <lucianogudas057@gmail.com>
2026-09-24 19:20:29 -0300

    Campos de email e senha

--- src/pages/home/login.tsx
+++ src/pages/home/login.tsx
@@ -1,7 +1,9 @@
  function Login() {
   return (
     <div>
-      <h1>Login do usuario</h1>
+      <h1>Login do usuario</h1> 
+      <h1>Email: usuario@gmail.com</h1>  
+      <h1>Senha: 123456</h1> 
     </div>
   )
 }
