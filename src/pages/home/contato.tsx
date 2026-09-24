500c2da4c6fd400ba2c1fc251dea90fb35a3ea3a
luciano <lucianogudas057@gmail.com>
2026-09-24 19:10:03 -0300

    campos do login

--- src/pages/home/contato.tsx
+++ src/pages/home/contato.tsx
@@ -1,7 +1,11 @@
  function Contato() {
   return (
     <div>
-      <h1>Contato</h1>
+      <h1>Contato</h1> 
+      <h1>Email: contato@gmail.com</h1> 
+      <h1>Telefone: (11) 99999-9999</h1> 
+
+      
     </div>
   )
 }
