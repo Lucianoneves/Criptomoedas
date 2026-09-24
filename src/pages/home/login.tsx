d7365f1106955513031a741c2f20152be40a86c8
luciano <lucianogudas057@gmail.com>
2026-09-24 19:32:42 -0300

    oisdfiojioef

--- src/pages/home/login.tsx
+++ src/pages/home/login.tsx
@@ -1,9 +1,7 @@
  function Login() {
   return (
     <div>
-      <h1>Login do usuario</h1> 
-      <h1>Email: usuario@gmail.com</h1>  
-      <h1>Senha: 123456</h1> 
+      <h1>Login do usuario</h1>
     </div>
   )
 }
